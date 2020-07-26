29f45afeadffd839e8d0b011d92b9dc5dd121ec2
Sabu George <sabu.george@elucidata.io>
2020-07-26 20:21:11 +0530

    [WIP]:waiting till the job is done

--- tests/run_check.sh
+++ tests/run_check.sh
@@ -5,4 +5,12 @@ for i in $(seq 1 1 $1)
 do
 	./check.sh $2 $tmpfile $3 &
 done
+
+echo "$(($1 * $2))"
+IFS=
+COMPLETE_DATA=$(cat $(pwd)/results/input/compute-jobs-10-user-2-jobs-each-1595688001-kKxxVH.log  | grep "status :" | wc -l)
+while [ $COMPLETE_DATA -ne "$(($1 * $2))" ]; do
+	sleep 5
+	echo "Spawning tasks"
+done
 echo "Updates on log $tmpfile"
