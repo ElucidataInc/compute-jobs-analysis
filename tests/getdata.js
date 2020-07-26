b5aa0a350efa3ec1f49ed6558b11e02c065d271a
Sabu George <sabu.george@elucidata.io>
2020-07-26 22:59:55 +0530

    [WIP]:adding the plots and checking before pushing

--- tests/getdata.js
+++ tests/getdata.js
@@ -52,5 +52,4 @@ const fullData = ${JSON.stringify(finalData, null, '  ')};
   fs.copyFileSync(`${path.resolve('.')}/index.html`, `${path.resolve('.')}/results/output/${folderName}/index.html`)
   fs.writeFileSync(`${path.resolve('.')}/results/output/${folderName}/result-${fileNamePrefix}.html`, root.toString());
   fs.writeFileSync(`${path.resolve('.')}/outputfile.txt`, `${path.resolve('.')}/results/output/${folderName}/result-${fileNamePrefix}.html`);
-  console.log(`${path.resolve('.')}/outputfile.txt`)
 })();
