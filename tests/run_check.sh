170f3c1b13248eac04602f6a0ce9357dee9a766a
Sabu George <sabu.george@elucidata.io>
2020-07-26 21:03:07 +0530

    [WIP]:testing pushing code back

--- tests/run_check.sh
+++ tests/run_check.sh
@@ -6,14 +6,11 @@ do
 	./check.sh $2 $tmpfile $3 &
 done
 
-echo "$(($1 * $2))"
-IFS=
 COMPLETE_DATA=$(cat $tmpfile  | grep "status :" | wc -l)
 while [ $COMPLETE_DATA -ne "$(($1 * $2))" ]; do
-	echo $COMPLETE_DATA
-	echo "$(($1 * $2))"
 	sleep 5
 	COMPLETE_DATA=$(cat $tmpfile  | grep "status :" | wc -l)
 	echo "Spawning tasks"
 done
 echo "Updates on log $tmpfile"
+export TEMP_POLLY_JOB_FILE=$tmpfile
