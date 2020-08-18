8937a9da876eec84e21577cffae9e34d5fb21a49
GeorgeSabu <sabu.george@elucidata.io>
2020-08-18 11:52:34 +0530

    Update update-env.js

--- tests/update-env.js
+++ tests/update-env.js
@@ -3,7 +3,7 @@ var envInstallPath = '/usr/local/lib/node_modules/@elucidatainc/pollycli/src/env
 var pollyEnv = JSON.parse(fs.readFileSync(envInstallPath));
 if (process.argv[2] == 'dev') {
   pollyEnv.baseApi = 'https://api.devpolly.elucidata.io';
-  pollyEnv.baseV2Api = 'https://v2.api.devpolly.elucidata.io;
+  pollyEnv.baseV2Api = 'https://v2.api.devpolly.elucidata.io';
   pollyEnv.dockerDomain =  'docker.devpolly.elucidata.io';
 } else if (process.argv[2] == 'test') {
   pollyEnv.baseApi = 'https://api.testpolly.elucidata.io';
